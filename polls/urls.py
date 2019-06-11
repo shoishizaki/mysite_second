480756618224ad2594d8124b3cb45db11f6f69eb
shoishizaki <shoishizaki1997@gmail.com>
2019-06-11 17:27:51 +0900

    Add index_page url

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    path('<int:id>/', views.index_page, name='index_page'),
     path('templates/', views.index_template, name='index_template'),
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
